5c3d0e31d8d9e61ea882458acb98a0b48a41aa67
Joseph <joseph@intentdriven.com>
2025-09-13 11:03:03 -0500

    More board logic and tests

--- Core/TicTacToeBoard.cs
+++ Core/TicTacToeBoard.cs
@@ -29,6 +29,49 @@
             Cells[row, column].Value = mark;
             return true;
         }
+        public bool PlaceComputerMark(char mark)
+        {
+            var emptyCells = new List<TicTacToeCell>();
+            foreach (var cell in Cells)
+            {
+                if (cell.Value == ' ')
+                    emptyCells.Add(cell);
+            }
+            if (emptyCells.Count == 0)
+                return false; // No empty cells
+            var random = new Random();
+            var chosenCell = emptyCells[random.Next(emptyCells.Count)];
+            chosenCell.Value = mark;
+            return true;
+        }
+        public bool PlaceComputerMark(char mark, int row, int column)
+        {
+            if (row < 0 
+                || row >= 3 
+                || column < 0 
+                || column >= 3)
+                throw new ArgumentOutOfRangeException("Row and Column must be between 0 and 2.");
+            if (mark != 'X' 
+                && mark != 'O')
+                throw new ArgumentException("Mark must be 'X' or 'O'.");
+            if (Cells[row, column].Value != ' ')
+                return false; // Cell already occupied
+            Cells[row, column].Value = mark;
+            return true;
+        }
+        public bool IsFull()
+        {
+            foreach (var cell in Cells)
+            {
+                if (cell.Value == ' ')
+                    return false;
+            }
+            return true;
+        }
+        public bool IsPlayerAboutToWin()
+        {
+            return false;
+        }
         public char CheckWinner()
         {
             // Check rows and columns
@@ -53,6 +96,13 @@
                 && Cells[1, 1].Value == Cells[2, 0].Value)
                 return Cells[0, 2].Value;
             return ' '; // No winner
+        }
+        public void Reset()
+        {
+            foreach (var cell in Cells)
+            {
+                cell.Value = ' ';
+            }
         }
     }
 }
--- UnitTests/TicTacToeBoardTests.cs
+++ UnitTests/TicTacToeBoardTests.cs
@@ -86,4 +86,95 @@ public class TicTacToeBoardTests
         board.PlaceMark(2, 2, 'O');
         board.CheckWinner().ShouldBe(' ');
     }
+    [TestMethod]
+    public void ShouldDetectFullBoard()
+    {
+        var board = new TicTacToeBoard();
+        board.PlaceMark(0, 0, 'X');
+        board.PlaceMark(0, 1, 'O');
+        board.PlaceMark(0, 2, 'X');
+        board.PlaceMark(1, 0, 'O');
+        board.PlaceMark(1, 1, 'X');
+        board.PlaceMark(1, 2, 'O');
+        board.PlaceMark(2, 0, 'O');
+        board.PlaceMark(2, 1, 'X');
+        board.PlaceMark(2, 2, 'O');
+        board.IsFull().ShouldBeTrue();
+        board = new TicTacToeBoard();
+        board.PlaceMark(0, 0, 'X');
+        board.IsFull().ShouldBeFalse();
+    }
+    [TestMethod]
+    public void ShouldResetBoard()
+    {
+        var board = new TicTacToeBoard();
+        board.PlaceMark(0, 0, 'X');
+        board.PlaceMark(1, 1, 'O');
+        board.Reset();
+        foreach (var cell in board.Cells)
+        {
+            cell.Value.ShouldBe(' ');
+        }
+    }
+    [TestMethod]
+    public void ShouldPlaceComputerMarkRandomly()
+    {
+        var board = new TicTacToeBoard();
+        board.PlaceComputerMark('O').ShouldBeTrue();
+        var occupiedCells = 0;
+        foreach (var cell in board.Cells)
+        {
+            if (cell.Value == 'O')
+                occupiedCells++;
+        }
+        occupiedCells.ShouldBe(1);
+    }
+    [TestMethod]
+    public void ShouldNotPlaceComputerMarkOnFullBoard()
+    {
+        var board = new TicTacToeBoard();
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                board.PlaceMark(i, j, 'X');
+            }
+        }
+        board.PlaceComputerMark('O').ShouldBeFalse();
+    }
+    [TestMethod]
+    public void ShouldPlaceComputerMarkAtSpecificPosition()
+    {
+        var board = new TicTacToeBoard();
+        var result = board.PlaceComputerMark('O', 2, 2);
+        result.ShouldBeTrue();
+        board.Cells[2, 2].Value.ShouldBe('O');
+    }
+    [TestMethod]
+    public void ShouldNotPlaceComputerMarkInOccupiedCell()
+    {
+        var board = new TicTacToeBoard();
+        board.PlaceMark(1, 1, 'X').ShouldBeTrue();
+        var result = board.PlaceComputerMark('O', 1, 1);
+        result.ShouldBeFalse();
+        board.Cells[1, 1].Value.ShouldBe('X');
+    }
+    [TestMethod]
+    public void ShouldThrowExceptionForInvalidComputerMarkPosition()
+    {
+        var board = new TicTacToeBoard();
+        Should.Throw<ArgumentOutOfRangeException>(
+            () => board.PlaceComputerMark('O', 3, 0));
+        Should.Throw<ArgumentOutOfRangeException>(
+            () => board.PlaceComputerMark('X', 0, -1));
+    }
+    [TestMethod]
+    public void ShouldThrowExceptionForInvalidComputerMark()
+    {
+        var board = new TicTacToeBoard();
+        Should.Throw<ArgumentException>(
+            () => board.PlaceComputerMark('A', 1, 1));
+        Should.Throw<ArgumentException>(
+            () => board.PlaceComputerMark(' ', 2, 2));
+    }
 }
