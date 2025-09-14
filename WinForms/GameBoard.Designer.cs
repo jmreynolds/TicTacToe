fa5e4d00278b40e8ee0731ac327387b86a2db2a8
Mark Reynolds <Mark@ProfReynolds.com>
2025-09-14 03:29:32 -0500

    Winforms is running
    look for: MER 2025-09-14
    the initial project compiles, with no functionality
    look at the interfaces

--- WinForms/GameBoard.Designer.cs
+++ WinForms/GameBoard.Designer.cs
@@ -30,49 +30,48 @@
         {
             btnExit = new Button();
             panel1 = new Panel();
-            btnCell00 = new Button();
+            btnCellToBeDeleted = new Button();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
             // btnExit
             // 
-            btnExit.Location = new Point(34, 1290);
-            btnExit.Margin = new Padding(9, 10, 9, 10);
+            btnExit.Location = new Point(12, 403);
             btnExit.Name = "btnExit";
-            btnExit.Size = new Size(346, 112);
+            btnExit.Size = new Size(121, 35);
             btnExit.TabIndex = 0;
             btnExit.Text = "Exit Tic Tac Toe";
             btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
             // 
             // panel1
             // 
             panel1.BorderStyle = BorderStyle.Fixed3D;
-            panel1.Controls.Add(btnCell00);
-            panel1.Location = new Point(97, 125);
-            panel1.Margin = new Padding(9, 10, 9, 10);
+            panel1.Controls.Add(btnCellToBeDeleted);
+            panel1.Location = new Point(234, 12);
             panel1.Name = "panel1";
-            panel1.Size = new Size(904, 1018);
+            panel1.Size = new Size(400, 400);
             panel1.TabIndex = 3;
             // 
-            // btnCell00
+            // btnCellToBeDeleted
             // 
-            btnCell00.Font = new Font("Comic Sans MS", 27.75F);
-            btnCell00.Location = new Point(37, 48);
-            btnCell00.Margin = new Padding(9, 10, 9, 10);
-            btnCell00.Name = "btnCell00";
-            btnCell00.Size = new Size(250, 250);
-            btnCell00.TabIndex = 2;
-            btnCell00.Text = "?";
-            btnCell00.UseVisualStyleBackColor = true;
+            btnCellToBeDeleted.Font = new Font("Comic Sans MS", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnCellToBeDeleted.Location = new Point(13, 15);
+            btnCellToBeDeleted.Name = "btnCellToBeDeleted";
+            btnCellToBeDeleted.Size = new Size(287, 78);
+            btnCellToBeDeleted.TabIndex = 2;
+            btnCellToBeDeleted.Text = "this is an example; it will be removed";
+            btnCellToBeDeleted.UseVisualStyleBackColor = true;
             // 
             // GameBoard
             // 
-            AutoScaleDimensions = new SizeF(20F, 48F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.LightSteelBlue;
-            ClientSize = new Size(2286, 1440);
+            ClientSize = new Size(800, 444);
             Controls.Add(panel1);
             Controls.Add(btnExit);
+            Margin = new Padding(1);
             Name = "GameBoard";
             Text = "Tic Tac Toe";
             panel1.ResumeLayout(false);
@@ -83,7 +82,7 @@
 
         private Button btnExit;
         private Panel panel1;
-        private Button btnCell00;
+        private Button btnCellToBeDeleted;
         
     }
 }
